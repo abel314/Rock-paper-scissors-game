faa8d0ba8bbaaf1f1f840466db35ee7ee6201b4c
Abel Oosterhuis <abel.oosterhuis@gmail.com>
2024-10-30 19:21:44 +0100

    Simlifying of JS code and adding a play again window

--- index.js
+++ index.js
@@ -100,7 +100,11 @@ function playRound(playerChoice) {
         playerCounter.textContent = `PLAYER: 5`;
         setTimeout(() => {
             endOfGameWindow.style.display = "flex";
+<<<<<<< HEAD
             result.textContent = "You won!"
+=======
+            result.textContent = "You won! 🎉"
+>>>>>>> 5bf440b (Simlifying of JS code and adding a play again window)
         }, 500);
 
 
